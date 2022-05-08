9cf3244f65c1ecd7825f126c55366a54852dc851
hogger <deuntlt@vip.qq.com>
2022-05-08 21:08:51 +0800

    Updates

---
+++ 252f366e-2cbf4d823bce16a06df4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[228],{2775:function(t,a,n){n.d(a,{h6V:function(){return e}});var c=n(1046);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-2cbf4d823bce16a06df4.js.map
